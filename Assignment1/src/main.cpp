4cbe718afec9d0f1673f968b6d58ff4d71d65eb6
Blake Boswell <blake.w.boswell@gmail.com>
2018-01-29 19:21:54 -0600

    Updated output format

--- Assignment1/src/main.cpp
+++ Assignment1/src/main.cpp
@@ -4,20 +4,20 @@
 #include "Point.h"
 
 int main() {
-    int numVertices = 51;
+    int numPoints = 51;
 
     int startL = 0;
-    int endL = startL + 6;
+    int endL = startL + 5;
     int startN = endL + 1;
-    int endN = startN + 10;
+    int endN = startN + 9;
     int startT = endN + 1;
-    int endT = startT + 8;
+    int endT = startT + 7;
     int startE = endT + 1;
-    int endE = startE + 12;
+    int endE = startE + 11;
     int startF = endE + 1;
-    int endF = startF + 10;
+    int endF = startF + 9;
 
-    Point points[numVertices];
+    Point points[numPoints];
     std::string pair;
     // Read points file
     std::ifstream file;
@@ -37,6 +37,7 @@ int main() {
     }
 
     bool loop = true;
+    int numVertices;
     char input;
     float xSize;
     float ySize;
@@ -46,99 +47,100 @@ int main() {
     float y;
     while(loop) {
         // prompt user for what letter they want to generate
-        std::cout << "Enter \"q\" at any time to quit the program." << std::endl;
-        std::cout << "What letter do you want to generate? (L, N, T, E, F)" << std::endl;
-        input;
+        std::cout << "What letter do you want to generate? (L, N, T, E, F)" << std::endl <<
+                  "Or enter  \"q\" to quit the program" << std::endl;
         std::cin >> input;
-        input = toupper(input);
-        // prompt user for size of model in the X & Y dimensions
-        if(input != 'Q') {
-            std::cout << "Enter X size" << std::endl;
-            std::cin >> xSize;
-            std::cout << "Enter Y size" << std::endl;
-            std::cin >> ySize;
+        if(isalpha(input)) {
+            input = toupper(input);
+            // prompt user for size of model in the X & Y dimensions
+            switch(input) {
+                case 'L':
+                    numVertices = 6;
+                    std::cout << "Enter X size" << std::endl;
+                    std::cin >> xSize;
+                    std::cout << "Enter Y size" << std::endl;
+                    std::cin >> ySize;
+                    originalWidth = 3;
+                    originalHeight = 5;
+                    std::cout << "poly2d" << numVertices << std::endl;
+                    for(int i = startL; i < endL + 1; i++) {
+                        x = points[i].getX() * (xSize/originalWidth);
+                        y = points[i].getY() * (ySize/originalHeight);
+                        std::cout << x << " " << y << std::endl;
+                    }
+                    break;
+                case 'N':
+                    numVertices = 10;
+                    std::cout << "Enter X size" << std::endl;
+                    std::cin >> xSize;
+                    std::cout << "Enter Y size" << std::endl;
+                    std::cin >> ySize;
+                    originalWidth = 5;
+                    originalHeight = 5;
+                    std::cout << "poly2d" << numVertices << std::endl;
+                    for(int i = startN; i < endN + 1; i++) {
+                        x = points[i].getX() * (xSize/originalWidth);
+                        y = points[i].getY() * (ySize/originalHeight);
+                        std::cout << x << " " << y << std::endl;
+                    }
+                    break;
+                case 'T':
+                    numVertices = 8;
+                    std::cout << "Enter X size" << std::endl;
+                    std::cin >> xSize;
+                    std::cout << "Enter Y size" << std::endl;
+                    std::cin >> ySize;
+                    originalWidth = 5;
+                    originalHeight = 5;
+                    std::cout << "poly2d" << numVertices << std::endl;
+                    for(int i = startT; i < endT + 1; i++) {
+                        x = points[i].getX() * (xSize/originalWidth);
+                        y = points[i].getY() * (ySize/originalHeight);
+                        std::cout << x << " " << y << std::endl;
+                    }
+                    break;
+                case 'E':
+                    numVertices = 12;
+                    std::cout << "Enter X size" << std::endl;
+                    std::cin >> xSize;
+                    std::cout << "Enter Y size" << std::endl;
+                    std::cin >> ySize;
+                    originalWidth = 3;
+                    originalHeight = 5;
+                    std::cout << "poly2d" << numVertices << std::endl;
+                    for(int i = startE; i < endE + 1; i++) {
+                        x = points[i].getX() * (xSize/originalWidth);
+                        y = points[i].getY() * (ySize/originalHeight);
+                        std::cout << x << " " << y << std::endl;
+                    }
+                    break;
+                case 'F':
+                    numVertices = 10;
+                    std::cout << "Enter X size" << std::endl;
+                    std::cin >> xSize;
+                    std::cout << "Enter Y size" << std::endl;
+                    std::cin >> ySize;
+                    originalWidth = 3;
+                    originalHeight = 5;
+                    std::cout << "poly2d" << numVertices << std::endl;
+                    for(int i = startF; i < endF + 1; i++) {
+                        x = points[i].getX() * (xSize/originalWidth);
+                        y = points[i].getY() * (ySize/originalHeight);
+                        std::cout << x << " " << y << std::endl;
+                    }
+                    break;
+                case 'Q':
+                    std::cout << "Terminating the program..." << std::endl;
+                    loop = false;
+                    break;
+                default:
+                    std::cout << "Invalid input: please enter one of the available letters(L, N, T, E, F)" << std::endl;
+                    break;
+            }
+        } else {
+            std::cout << "Invalid input: please enter a letter" << std::endl;
         }
-        switch(input) {
-            case 'L':
-                // numVertices = 7
-                originalWidth = 3;
-                originalHeight = 5;
-                for(int i = startL; i < endL + 1; i++) {
-                    if(i == 0)
-                        std::cout << "From" << std::endl;
-                    x = points[i].getX() * (xSize/originalWidth);
-                    y = points[i].getY() * (ySize/originalHeight);
-                    std::cout << "(" << x << ", " << y << ")" << std::endl;
-                    if(i != endL)
-                        std::cout << "To" << std::endl;
-                }
-                break;
-            case 'N':
-                // numVertices = 11
-                originalWidth = 5;
-                originalHeight = 5;
-                for(int i = startN; i < endN + 1; i++) {
-                    if(i == 0)
-                        std::cout << "From" << std::endl;
-                    x = points[i].getX() * (xSize/originalWidth);
-                    y = points[i].getY() * (ySize/originalHeight);
-                    std::cout << "(" << x << ", " << y << ")" << std::endl;
-                    if(i != endN)
-                        std::cout << "To" << std::endl;
-                }
-                break;
-            case 'T':
-                // numVertices = 9
-                originalWidth = 5;
-                originalHeight = 5;
-                for(int i = startT; i < endT + 1; i++) {
-                    if(i == 0)
-                        std::cout << "From" << std::endl;
-                    x = points[i].getX() * (xSize/originalWidth);
-                    y = points[i].getY() * (ySize/originalHeight);
-                    std::cout << "(" << x << ", " << y << ")" << std::endl;
-                    if(i != endT)
-                        std::cout << "To" << std::endl;
-                }
-                break;
-            case 'E':
-                // numVertices = 13
-                originalWidth = 3;
-                originalHeight = 5;
-                for(int i = startE; i < endE + 1; i++) {
-                    if(i == 0)
-                        std::cout << "From" << std::endl;
-                    x = points[i].getX() * (xSize/originalWidth);
-                    y = points[i].getY() * (ySize/originalHeight);
-                    std::cout << "(" << x << ", " << y << ")" << std::endl;
-                    if(i != endE)
-                        std::cout << "To" << std::endl;
-                }
-                break;
-            case 'F':
-                // numVertices = 11
-                originalWidth = 3;
-                originalHeight = 5;
-                for(int i = startF; i < endF + 1; i++) {
-                    if(i == 0)
-                        std::cout << "From" << std::endl;
-                    x = points[i].getX() * (xSize/originalWidth);
-                    y = points[i].getY() * (ySize/originalHeight);
-                    std::cout << "(" << x << ", " << y << ")" << std::endl;
-                    if(i != endF)
-                        std::cout << "To" << std::endl;
-                }
-                break;
-            case 'Q':
-                std::cout << "Terminating the program..." << std::endl;
-                loop = false;
-                break;
-            default:
-                std::string trash;
-                std::cout << "Invalid input: please enter one of the available letters(L, N, T, E, F)" << std::endl;
-                std::cin >> trash;
-                break;
-        }
+
     }
 
     return 0;
